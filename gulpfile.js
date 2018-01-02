56fcd9d5e263a276a6e0c0f85310bdc63bfe8fad
Richard Souto <contato@gritoweb.com.br>
2018-01-02 14:26:14 -0200

    Moving materialize js to vendor

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,54 @@ var gulp          = require('gulp'),
     uglify        = require('gulp-uglify-es').default;
 
 //js path
-var jsfiles = '_assets/js/**/*.js';
+var jsfiles = [
+  "_assets/js/vendor/initial.js",
+  "_assets/js/vendor/jquery.easing.1.4.js",
+  "_assets/js/vendor/animation.js",
+  "_assets/js/vendor/velocity.min.js",
+  "_assets/js/vendor/hammer.min.js",
+  "_assets/js/vendor/jquery.hammer.js",
+  "_assets/js/vendor/global.js",
+  "_assets/js/vendor/collapsible.js",
+  "_assets/js/vendor/dropdown.js",
+  "_assets/js/vendor/modal.js",
+  "_assets/js/vendor/materialbox.js",
+  "_assets/js/vendor/parallax.js",
+  "_assets/js/vendor/tabs.js",
+  "_assets/js/vendor/tooltip.js",
+  "_assets/js/vendor/waves.js",
+  "_assets/js/vendor/toasts.js",
+  "_assets/js/vendor/sideNav.js",
+  "_assets/js/vendor/scrollspy.js",
+  "_assets/js/vendor/forms.js",
+  "_assets/js/vendor/slider.js",
+  "_assets/js/vendor/cards.js",
+  "_assets/js/vendor/chips.js",
+  "_assets/js/vendor/pushpin.js",
+  "_assets/js/vendor/buttons.js",
+  "_assets/js/vendor/transitions.js",
+  "_assets/js/vendor/scrollFire.js",
+  "_assets/js/vendor/date_picker/picker.js",
+  "_assets/js/vendor/date_picker/picker.date.js",
+  "_assets/js/vendor/date_picker/picker.time.js",
+  "_assets/js/vendor/character_counter.js",
+  "_assets/js/vendor/carousel.js",
+  "_assets/js/vendor/tapTarget.js",
+  "_assets/js/main.js",
+];
 
 //generate css
 gulp.task('sass', function(){
     gulp.src('_assets/scss/style.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(autoprefixer({
-        browsers: ['last 40 versions']
+        browsers: [
+          'last 2 versions',
+          'Chrome >= 30',
+          'Firefox >= 30',
+          'ie >= 10',
+          'Safari >= 8'
+        ]
       }))
       .pipe(rename('style.css'))
       .pipe(gulp.dest('css/'))
@@ -28,6 +68,7 @@ gulp.task('sass', function(){
 //generate js
 gulp.task('scripts', function (){
   return gulp.src(jsfiles)
+    .pipe(concat('main.js'))
     .pipe(sourcemaps.init())
     .pipe(rename('main.js'))
     .pipe(sourcemaps.write())
